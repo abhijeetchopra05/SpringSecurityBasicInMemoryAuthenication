e038511d4623722f648f8330fd9a2612b59392e5
Abhijeet Chopra <abhijeetchopra@pop-os.localdomain>
2018-10-23 13:25:24 +0530

    initial commit

---
+++ basicInMemoryAuthentication/src/main/java/com/springsecurity/basicInMemoryAuthentication/BasicInMemoryAuthenticationApplication.java
@@ -0,0 +1,12 @@
+package com.springsecurity.basicInMemoryAuthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicInMemoryAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicInMemoryAuthenticationApplication.class, args);
+	}
+}
---
+++ basicInMemoryAuthentication/src/test/java/com/springsecurity/basicInMemoryAuthentication/BasicInMemoryAuthenticationApplicationTests.java
@@ -0,0 +1,16 @@
+package com.springsecurity.basicInMemoryAuthentication;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BasicInMemoryAuthenticationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
